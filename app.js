a5b61b557f38e010903d21f2c8b9a1fe9a9813b4
Raj Rabadiya <raj.rabadiya4427@gmail.com>
2026-08-03 16:56:04 +0530

    Add tailwind css

--- app.js
+++ app.js
@@ -1088,10 +1088,4 @@ function setCurrentYear() {
 }
 
 
-const hamburgerBtn = document.getElementById('hamburgerBtn');
-const navMenu = document.getElementById('navMenu');
-
-hamburgerBtn.addEventListener('click', () => {
-    navMenu.classList.toggle('hidden');
-});
 
